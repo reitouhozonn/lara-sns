08d00d88407234a7b81826c01f3b443ae9e12311
teionhozonn <teionhozonn@gmail.com>
2021-01-30 23:44:49 +0900

    0130

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $articles = [
+            (object) [
+                'id' => 1,
+                'title' => 'タイトル1',
+                'body' => '本文1',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 1,
+                    'name' => 'ユーザー名1',
+                ],
+            ],
+            (object) [
+                'id' => 2,
+                'title' => 'タイトル2',
+                'body' => '本文2',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 2,
+                    'name' => 'ユーザー名2',
+                ],
+            ],
+            (object) [
+                'id' => 3,
+                'title' => 'タイトル3',
+                'body' => '本文3',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 3,
+                    'name' => 'ユーザー名3',
+                ],
+            ],
+        ];
+
+        return view('articles.index', ['articles' => $articles]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,36 @@
+@extends('app')
+
+@section('title', '記事一覧')
+
+@section('content')
+  @include('nav')
+  <div class="container">
+    @foreach($articles as $article)
+    <div class="card mt-3">
+      <div class="card-body d-flex flex-row">
+        <i class="fas fa-user-circle fa-3x mr-1"></i>
+        <div>
+          <div class="font-weight-bold">
+            <!-- ユーザー名 -->
+            {{ $article->user->name }}
+          </div>
+          <div class="font-weight-lighter">
+            <!-- 日時 -->
+            {{ $article->created_at->format('Y/m/d H:i')}}
+          </div>
+        </div>
+      </div>
+      <div class="card-body pt-0 pb-2">
+        <h3 class="h4 card-title">
+          <!-- 記事タイトル -->
+          {{ $article->title }}
+        </h3>
+        <div class="card-text">
+          <!-- 記事本文 -->
+          {!! nl2br(e( $article->body )) !!}
+        </div>
+      </div>
+    </div>
+    @endforeach
+  </div>
+@endsection
