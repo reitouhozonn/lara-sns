ad4b705d1a4d8ccf81c4a3f91860f5df9397e789
teionhozonn <teionhozonn@gmail.com>
2021-02-09 15:10:27 +0900

    0209

--- database/migrations/2021_01_30_143904_create_articles_table.php
+++ database/migrations/2021_01_30_143904_create_articles_table.php
@@ -18,9 +18,10 @@ class CreateArticlesTable extends Migration
             $table->string('title');
             $table->text('body');
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
-              // ->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            // ->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
