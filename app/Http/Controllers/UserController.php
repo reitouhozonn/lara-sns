5cc8f6b3054c6de56345d2c04a4853d5309098df
teionhozonn <teionhozonn@gmail.com>
2021-02-27 13:45:46 +0900

    0227

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UserController extends Controller
+{
+    public function show(string $name)
+    {
+      $user = User::where('name', $name)->first();
+
+      $articles = $user->articles->sortByDesc('created_at');
+
+      return view('users.show', [
+        'user' => $user,
+        'articles' => $articles,
+      ]);
+    }
+
+    public function likes(string $name)
+    {
+      $user = User::wherez('name', $name)->first();
+
+      $articles = $user->likes->sortByDesc('created_at');
+
+      return viewa('users.likes', [
+        'user' => $user,
+        'articles' => $articles,
+      ]);
+    }
+
+    public function follow(Request $request, string $name)
+    {
+      $user = User::where('name', $name)->first();
+
+      if ($user->id === $request->user()->id) {
+        return about('404', 'Cannot follow yourself.');
+      }
+
+      $request->user()->followings()->detach($user);
+      $request->user()->followings()->attach($user);
+
+      return ['name' => $name];
+    }
+
+    public function unfollow(Request $request, string $name)
+    {
+      $user = User::where('name', $name)->first();
+
+      if ($user->id === $request->user()->id) {
+        return about('404', 'Cannot follow yourself.');
+      }
+
+      $request->user()->followings()->detach($user);
+
+      return ['name' => $name];
+    }
+
+
+
+
+}
--- app/User.php
+++ app/User.php
@@ -5,6 +5,9 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 use App\Mail\BareMail;
 use App\Notifications\PasswordResetNotification;
@@ -45,5 +48,42 @@ class User extends Authenticatable
     public function sendPasswordResetNotification($token)
     {
       $this->notify(new PasswordResetNotification($token, new BareMail()));
+    }
+
+    public function articles(): HasMany
+    {
+      return $this->hasMany('App\Article');
+    }
+
+    public function followers(): BelongsToMany
+    {
+      return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
+    }
+
+    public function followings(): BelongsToMany
+    {
+      return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
+    }
+
+    public function likes(): BelongsToMany
+    {
+      return $this->belongsToMany('App\Article', 'likes')->withTimestamps();
+    }
+
+    public function isFollowedBy(?User $user): bool
+    {
+      return $user
+      ? (bool)$this->followers->where('id', $user->id)->count()
+      : false;
+    }
+
+    public function getCountFollowersAttribute(): int
+    {
+      return $this->followers->count();
+    }
+
+    public function getCountFollowingsAttribute(): int
+    {
+      return $this->followings->count();
     }
 }
