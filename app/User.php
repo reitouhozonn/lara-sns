58c1f8cdc73b96a67eb3840ca6180673504141ff
teionhozonn <teionhozonn@gmail.com>
2021-10-18 20:47:30 +0900

    check

--- app/User.php
+++ app/User.php
@@ -14,76 +14,86 @@ use App\Notifications\PasswordResetNotification;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
 
-    public function sendPasswordResetNotification($token)
-    {
-      $this->notify(new PasswordResetNotification($token, new BareMail()));
-    }
+  public function sendPasswordResetNotification($token)
+  {
+    $this->notify(new PasswordResetNotification($token, new BareMail()));
+  }
 
-    public function articles(): HasMany
-    {
-      return $this->hasMany('App\Article');
-    }
+  public function articles(): HasMany
+  {
+    return $this->hasMany('App\Article');
+  }
 
-    public function followers(): BelongsToMany
-    {
-      return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
-    }
+  public function followers(): BelongsToMany
+  {
+    return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
+  }
 
-    public function followings(): BelongsToMany
-    {
-      return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
-    }
+  public function followings(): BelongsToMany
+  {
+    return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
+  }
 
-    public function likes(): BelongsToMany
-    {
-      return $this->belongsToMany('App\Article', 'likes')->withTimestamps();
-    }
+  public function likes(): BelongsToMany
+  {
+    return $this->belongsToMany('App\Article', 'likes')->withTimestamps();
+  }
 
-    public function isFollowedBy(?User $user): bool
-    {
-      return $user
+  public function isFollowedBy(?User $user): bool
+  {
+    return $user
       ? (bool)$this->followers->where('id', $user->id)->count()
       : false;
-    }
+  }
 
-    public function getCountFollowersAttribute(): int
-    {
-      return $this->followers->count();
-    }
+  public function getCountFollowersAttribute(): int
+  {
+    return $this->followers->count();
+  }
 
-    public function getCountFollowingsAttribute(): int
-    {
-      return $this->followings->count();
-    }
+  public function getCountFollowingsAttribute(): int
+  {
+    return $this->followings->count();
+  }
+
+  /**
+   * Undocumented function
+   *
+   * @return HasMany
+   */
+  public function solditems(): HasMany
+  {
+    return $this->hasMany(Item::class, 'seller_id');
+  }
 }
